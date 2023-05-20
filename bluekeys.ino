80c7f06816e56ab677d492d2e21398f19fb09a24
thelaserguy <125558835+thelaserguy@users.noreply.github.com>
2023-05-20 17:45:48 -0400

    Update bluekeys.ino
    # Enhanced the code to include dynamic arrays for key pins and key values,
    # Implemented a more reliable debouncing mechanism

--- bluekeys.ino
+++ bluekeys.ino
@@ -2,8 +2,8 @@
 
 #define NUM_KEYS 10
 
-const int keyPins[NUM_KEYS] = {18, 19, 21, 22, 23, 25, 26, 27, 32};  // array of pins where keys are connected
-const uint8_t keyValues[NUM_KEYS] = {KEY_F1, KEY_F2, KEY_F3, KEY_F4, KEY_F5, KEY_F6, KEY_F7, KEY_F8, KEY_F9, KEY_F10};  // change this to match your keys
+int keyPins[NUM_KEYS] = {18, 19, 21, 22, 23, 25, 26, 27, 32};
+uint8_t keyValues[NUM_KEYS] = {KEY_F1, KEY_F2, KEY_F3, KEY_F4, KEY_F5, KEY_F6, KEY_F7, KEY_F8, KEY_F9, KEY_F10};
 
 BleKeyboard bleKeyboard;
 
@@ -12,22 +12,28 @@ void setup() {
   Serial.println("MacroKeypad Connected");
 
   for (int i = 0; i < NUM_KEYS; i++) {
-    pinMode(keyPins[i], INPUT_PULLUP);  // configure key pins as inputs with pullup resistors
+    pinMode(keyPins[i], INPUT_PULLUP);
   }
-  
+
   bleKeyboard.begin();
 }
 
 void loop() {
   if (bleKeyboard.isConnected()) {
     for (int i = 0; i < NUM_KEYS; i++) {
-      if (digitalRead(keyPins[i]) == LOW) {  // if key is pressed (LOW because of pullup resistor)
+      if (digitalRead(keyPins[i]) == LOW) {
         bleKeyboard.write(KEY_LEFT_CTRL);
         bleKeyboard.write(KEY_LEFT_SHIFT);
-        bleKeyboard.write(keyValues[i]);  // send key combination
-        delay(200);  // basic & crude debouncing - gonna want to improve this
+        bleKeyboard.write(keyValues[i]);
+
+        delay(50);
+
+        while (digitalRead(keyPins[i]) == LOW) {
+          delay(10);
+        }
       }
     }
   }
-  delay(10);  // short delay to avoid hogging the CPU
+
+  delay(10);
 }
